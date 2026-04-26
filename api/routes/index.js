a94172007a0aa06a251830186a57f33bbe6a6550
Samed <96351478+sameddartilmak@users.noreply.github.com>
2026-04-26 19:31:01 +0300

    users ve rol modelleri oluşturuldu

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const config = require('../config');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Samed' });
+  res.render('index', { title: 'Samed',config });
 });
 
 module.exports = router;
